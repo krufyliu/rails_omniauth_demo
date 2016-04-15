0a6a36c2596f00c4738090cd5f7d596b7353a46a
liujun <lj90214@gmail.com>
2016-04-15 18:25:09 +0800

    another way to avoid omniauth csrf error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+  before_action :set_provider_state
+    
   def index
+  end
+
+  private
+  def set_provider_state
+    session['omniauth.state'] = SecureRandom.hex(24)
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,10 +3,6 @@ require 'omniauth/strategies/coding'
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], {
-    provider_ignores_state: true
-  }
-  provider :coding, ENV['CODING_ID'], ENV['CODING_SECRET'], {
-    provider_ignores_state: true
-  }
+  provider :github, ENV['GITHUB_ID'], ENV['github_secret']
+  provider :coding, ENV['CODING_ID'], ENV['CODING_SECRET']
 end
